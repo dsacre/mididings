f2873de272989ab897ef0a39eddc56f747cceff4
Dominic Sacré <dominic.sacre@gmx.de>
2008-07-24 02:51:31 +0000

    improved Print() indentation, so everything lines up nicely
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@79 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- mididings/units.py
+++ mididings/units.py
@@ -309,31 +309,47 @@ class Print(_CallBase):
     PORTNAMES_IN   = 1
     PORTNAMES_OUT  = 2
 
+    max_name_length = -1
+    max_portname_length = -1
+
     def __init__(self, name=None, types=_event.ANY, portnames=PORTNAMES_NONE):
         self.name = name
         self.types = types
-        self.ports = portnames
+        self.portnames = portnames
+
+        # to be calculated later
+        self.ports = None
+
+        # find maximum name length
+        if name:
+            Print.max_name_length = max(Print.max_name_length, len(name))
+
         _CallBase.__init__(self, self.do_print, True, True)
 
     def do_print(self, ev):
-        # delayed 'til first use, because _main.TheSetup doesn't yet exist during __init__
-        if not hasattr(self, 'portnames'):
-            if self.ports == Print.PORTNAMES_IN:
-                self.portnames = _main.TheSetup.in_ports
-            elif self.ports == Print.PORTNAMES_OUT:
-                self.portnames = _main.TheSetup.out_ports
+        # get list of port names to be used
+        # (delayed 'til first use, because _main.TheSetup doesn't yet exist during __init__)
+        if self.ports == None:
+            if self.portnames == Print.PORTNAMES_IN:
+                self.ports = _main.TheSetup.in_ports
+            elif self.portnames == Print.PORTNAMES_OUT:
+                self.ports = _main.TheSetup.out_ports
             else:
-                self.portnames = []
+                self.ports = []
 
-            if len(self.portnames):
-                self.portname_length = max((len(p) for p in self.portnames))
-            else:
-                self.portname_length = 0
+        # find maximum port name length (delayed for the same reason as above)
+        if Print.max_portname_length == -1:
+            all_ports = _main.TheSetup.in_ports + _main.TheSetup.out_ports
+            Print.max_portname_length = max((len(p) for p in all_ports))
 
         if ev.type_ & self.types:
             if self.name:
-                print self.name + ":",
-            print ev.to_string(self.portnames, self.portname_length)
+                print '%-*s' % (Print.max_name_length + 1, self.name + ':'),
+            elif Print.max_name_length != -1:
+                # no name, but names used elsewhere, so indent appropriately
+                print ' ' * (Print.max_name_length + 1),
+
+            print ev.to_string(self.ports, Print.max_portname_length)
 
 
 class PrintString(_CallBase):
