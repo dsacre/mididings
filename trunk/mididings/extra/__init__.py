2418b0530040db22f402ff6c55367bd921e790fc
Dominic Sacré <dominic.sacre@gmx.de>
2008-08-29 13:43:16 +0000

    added mididings/extra to manifest import * from all submodules in extra/__init__.py
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@120 5e725e9c-a60a-11dc-ba67-4190e2e3db76

---
+++ trunk/mididings/extra/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# mididings
+#
+# Copyright (C) 2008  Dominic Sacré  <dominic.sacre@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
+from harmonizer import *
+from suppress_pc import *
+from pedal_noteoff import *
+from black_keys import *
--- trunk/setup.py
+++ trunk/setup.py
@@ -16,7 +16,7 @@ sysconfig.get_config_vars()['CFLAGS'] = ' '.join(cv_opt.split())
 
 setup(
     name = 'mididings',
-    version = '20080817',
+    version = '20080828',
     author = 'Dominic Sacre',
     author_email = 'dominic.sacre@gmx.de',
     url = 'http://das.nasophon.de/mididings',
