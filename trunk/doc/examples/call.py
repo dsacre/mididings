417ba4b2faebc209a4b75b051ba4b9257fa1bc54
Dominic Sacré <dominic.sacre@gmx.de>
2008-09-05 01:33:45 +0000

    updated documentation completely rewritten examples
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@128 5e725e9c-a60a-11dc-ba67-4190e2e3db76

---
+++ trunk/doc/examples/call.py
@@ -0,0 +1,21 @@
+#
+# call.py - shows how to process MIDI events in Python
+#
+# This is a curious little script that inverts the velocity of note-on events.
+# The harder you press the keys, the quieter the sound will become :)
+#
+# Events on channels other than channel 1 are discarded.
+#
+
+from mididings import *
+
+def invert(ev):
+    if ev.channel == 1:
+        if ev.type_ == NOTEON:
+            ev.velocity = 128 - ev.velocity
+        return ev
+    else:
+        return None
+
+
+run(Call(invert))
