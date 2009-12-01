eae1cac7a6a36add1d96f2a88c43cda82629a098
Dominic Sacré <dominic.sacre@gmx.de>
2009-12-01 15:19:02 +0000

    new ring buffer that can be safely used with C++ objects
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@236 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/src/util/ringbuffer.hh
+++ trunk/src/util/ringbuffer.hh
@@ -12,52 +12,91 @@
 
 
 #include <boost/noncopyable.hpp>
-
-#include <jack/ringbuffer.h>
+#include <glib.h>
 
 
 namespace das {
 
-
-template<typename T>
-class ringbuffer
-  : boost::noncopyable
+/*
+ * lock-free ring buffer.
+ * inspired by and partly copied from Raul::RingBuffer by Dave Robillard
+ */
+template <typename T>
+class ringbuffer : boost::noncopyable
 {
   public:
-    ringbuffer(std::size_t size) {
-        _rb = jack_ringbuffer_create(size * sizeof(T));
+    ringbuffer(size_t size)
+      : _size(size)
+      , _buf(new T[size])
+    {
+        reset();
     }
 
-    ~ringbuffer() {
-        jack_ringbuffer_free(_rb);
-    }
-
-    bool write(T const & item) {
-        if (!write_space()) return false;
-        jack_ringbuffer_write(_rb, reinterpret_cast<char const *>(&item), sizeof(T));
-        return true;
-    }
-
-    bool read(T & item) {
-        if (!read_space()) return false;
-        jack_ringbuffer_read(_rb, reinterpret_cast<char *>(&item), sizeof(T));
-        return true;
-    }
-
-    std::size_t write_space() {
-        return jack_ringbuffer_write_space(_rb) / sizeof(T);
-    }
-
-    std::size_t read_space() {
-        return jack_ringbuffer_read_space(_rb) / sizeof(T);
+    virtual ~ringbuffer() {
+        delete[] _buf;
     }
 
     void reset() {
-        jack_ringbuffer_reset(_rb);
+        g_atomic_int_set(&_write_ptr, 0);
+        g_atomic_int_set(&_read_ptr, 0);
     }
 
-  private:
-    jack_ringbuffer_t * _rb;
+    size_t write_space() const {
+        const size_t w = g_atomic_int_get(&_write_ptr);
+        const size_t r = g_atomic_int_get(&_read_ptr);
+
+        if (w > r) {
+            return ((r - w + _size) % _size) - 1;
+        } else if (w < r) {
+            return (r - w) - 1;
+        } else {
+            return _size - 1;
+        }
+    }
+
+    size_t read_space() const {
+        const size_t w = g_atomic_int_get(&_write_ptr);
+        const size_t r = g_atomic_int_get(&_read_ptr);
+
+        if (w > r) {
+            return w - r;
+        } else {
+            return (w - r + _size) % _size;
+        }
+    }
+
+    size_t capacity() const {
+        return _size;
+    }
+
+    bool read(T & dst) {
+        if (read_space()) {
+            const size_t priv_read_ptr = g_atomic_int_get(&_read_ptr);
+            dst = _buf[priv_read_ptr];
+            g_atomic_int_set(&_read_ptr, (priv_read_ptr + 1) % _size);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    bool write(const T & src) {
+        if (write_space()) {
+            const size_t priv_write_ptr = g_atomic_int_get(&_write_ptr);
+            _buf[priv_write_ptr] = src;
+            g_atomic_int_set(&_write_ptr, (priv_write_ptr + 1) % _size);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+protected:
+    mutable int _write_ptr;
+    mutable int _read_ptr;
+
+    size_t _size;
+    T*     _buf;
 };
 
 
