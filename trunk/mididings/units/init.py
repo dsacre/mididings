79e7c6fbae49f7bea1404afe3c9c9d7ad0dc1bcc
Dominic Sacré <dominic.sacre@gmx.de>
2010-03-14 18:25:42 +0000

    added OutputTemplate() added pan, expression and ctrls parameters to Output()
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@326 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/mididings/units/init.py
+++ trunk/mididings/units/init.py
@@ -10,11 +10,14 @@
 # (at your option) any later version.
 #
 
-from mididings.units.base import _Unit, Fork, _unit_repr
+from mididings.units.base import _Unit, Fork, Chain, _unit_repr, _UNIT_TYPES
 from mididings.units.generators import Program, Ctrl
 from mididings.units.modifiers import Port, Channel
 
 import mididings.misc as _misc
+
+import functools as _functools
+import copy as _copy
 
 
 class _Init(_Unit):
@@ -31,7 +34,7 @@ def InitAction(action):
     return Init(action)
 
 
-def Output(port, channel, program=None, volume=None):
+def Output(port, channel, program=None, volume=None, pan=None, expression=None, ctrls={}):
     if isinstance(program, tuple):
         bank, program = program
     else:
@@ -48,8 +51,39 @@ def Output(port, channel, program=None, volume=None):
 
     if volume != None:
         init.append(Ctrl(port, channel, 7, volume))
+    if pan != None:
+        init.append(Ctrl(port, channel, 10, volume))
+    if expression != None:
+        init.append(Ctrl(port, channel, 11, volume))
+
+    for k, v in ctrls.items():
+        init.append(Ctrl(port, channel, k, v))
 
     return Fork([
         Init(init),
         Port(port) >> Channel(channel)
     ])
+
+
+class OutputTemplate(object):
+    def __init__(self, *args, **kwargs):
+        self.partial = _functools.partial(Output, *args, **kwargs)
+        self.before = []
+        self.after = []
+
+    def __call__(self, *args, **kwargs):
+        return Chain(self.before) >> self.partial(*args, **kwargs) >> Chain(self.after)
+
+    def __rshift__(self, other):
+        if not isinstance(other, _UNIT_TYPES):
+            return NotImplemented
+        r = _copy.copy(self)
+        r.after = self.after + [other]
+        return r
+
+    def __rrshift__(self, other):
+        if not isinstance(other, _UNIT_TYPES):
+            return NotImplemented
+        r = _copy.copy(self)
+        r.before = [other] + self.before
+        return r
