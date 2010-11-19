4cb07e6260fc815f37b9f6b1c269388bed738436
Dominic Sacré <dominic.sacre@gmx.de>
2010-11-19 10:16:09 +0000

    added license section to README added license header to test/unit.py updated docs
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@376 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/test/unit.py
+++ trunk/test/unit.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# mididings
+#
+# Copyright (C) 2008-2010  Dominic Sacré  <dominic.sacre@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+
 import unittest
 
 from mididings import *
