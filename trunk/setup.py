59c8a058a4034832c728c1ef80e1c456a5fbf1c6
Dominic Sacré <dominic.sacre@gmx.de>
2010-11-19 23:50:59 +0000

    release 20101119
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@380 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/setup.py
+++ trunk/setup.py
@@ -120,7 +120,7 @@ sysconfig.customize_compiler = my_customize_compiler
 
 setup(
     name = 'mididings',
-    version = '20100602',
+    version = '20101119',
     author = 'Dominic Sacre',
     author_email = 'dominic.sacre@gmx.de',
     url = 'http://das.nasophon.de/mididings/',
