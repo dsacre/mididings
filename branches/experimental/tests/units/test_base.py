04cddf1df32a0ca9e79b9408f4b631e3bae70b88
Dominic Sacré <dominic.sacre@gmx.de>
2012-03-19 00:02:59 +0000

    add test case for unit execution order
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@445 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- branches/experimental/tests/units/test_base.py
+++ branches/experimental/tests/units/test_base.py
@@ -124,3 +124,24 @@ class BaseTestCase(tests.helpers.MididingsTestCase):
             self.make_event(CTRL, ctrl=23, value=42): True,
             self.make_event(CTRL, ctrl=42, value=123): False,
         })
+
+    def test_unit_order(self):
+        def me_impl(ev, n):
+            order.append(n)
+            return ev
+
+        def me(n):
+            return Process(lambda ev: me_impl(ev, n))
+
+        order = []
+        p = ([ me(1), me(2) ] >> me(3) >>
+             [ me(4), me(5) >> [ me(6), me(7) ] >> me(8) ] >> me(9))
+        self.run_patch(p, self.make_event())
+        self.assertEqual(order, [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+        order = []
+        p = (Fork([ me(1), me(2) ], False) >> me(3) >>
+             Fork([ me(4), me(5) >> Fork([ me(6), me(7) ], False) >> me(8) ], False) >> me(9))
+
+        self.run_patch(p, self.make_event())
+        self.assertEqual(order, [1, 2, 3, 3, 4, 5, 6, 7, 8, 8, 4, 5, 6, 7, 8, 8, 9, 9, 9, 9, 9, 9])
