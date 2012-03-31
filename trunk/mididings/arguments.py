ade750973b984a79204ce72b660882f690b2181f
Dominic Sacré <dominic.sacre@gmx.de>
2012-03-31 22:23:55 +0000

    replace file header
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@481 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/mididings/arguments.py
+++ trunk/mididings/arguments.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# mididings
+#
+# Copyright (C) 2008-2012  Dominic Sacré  <dominic.sacre@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
 
 import mididings.misc as misc
 
