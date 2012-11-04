f77196e4ef3445173d67f859ed9dff23cdba579a
Dominic Sacré <dominic.sacre@gmx.de>
2012-11-04 20:48:26 +0000

    update copyright date
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@582 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/src/util/ringbuffer.hh
+++ trunk/src/util/ringbuffer.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009  Dominic Sacré  <dominic.sacre@gmx.de>
+ * Copyright (C) 2009-2012  Dominic Sacré  <dominic.sacre@gmx.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
