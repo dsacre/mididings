5eaa0595db7a0fc6ced58bd66be2ae01255b1c33
Dominic Sacré <dominic.sacre@gmx.de>
2014-04-30 08:03:06 +0000

    update copyright dates
    git-svn-id: svn+ssh://kobol/srv/svn/mididings@601 5e725e9c-a60a-11dc-ba67-4190e2e3db76

--- trunk/mididings/extra/engine.py
+++ trunk/mididings/extra/engine.py
@@ -2,7 +2,7 @@
 #
 # mididings
 #
-# Copyright (C) 2008-2012  Dominic Sacré  <dominic.sacre@gmx.de>
+# Copyright (C) 2008-2014  Dominic Sacré  <dominic.sacre@gmx.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
